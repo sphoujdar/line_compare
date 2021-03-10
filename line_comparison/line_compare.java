871a48c72cf888ab5338309d4d7b6c96824cb6ed
Shubham Phoujdar <sm.phoujdar@gmail.com>
2021-03-10 16:20:25 +0530

    Refactored : New formatted print message.

--- line_comparison/line_compare.java
+++ line_comparison/line_compare.java
@@ -6,7 +6,7 @@ public class line_compare {
 		
 			line line1 = new line(0,0,3,4);
 			
-			System.out.printf("Points Provided - \nP1 [X1,Y1] = [%f,%f] \n"
+			System.out.printf("Points provided - \nP1 [X1,Y1] = [%f,%f] \n"
 					+ "P2 [X1,Y1] = [%f,%f] \n"
 					+ "Length of provided Line is : %f \n",
 					line1.x1, line1.y1, line1.x2, line1.y2, line1.length_of_line);
