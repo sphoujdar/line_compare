9d8cac95ef0db86f399c15fbf63c8a71102a9dde
Shubham Phoujdar <sm.phoujdar@gmail.com>
2021-03-10 15:09:35 +0530

    Refarctored : Message Updated

--- line_comparison/line_compare.java
+++ line_comparison/line_compare.java
@@ -2,6 +2,6 @@ package line_comparison;
 
 public class line_compare {
 		public static void main(String[] args) {
-			System.out.println("Welcome to Line Comparison Computation Program.");
+			System.out.println("Welcome to Line Comparison Computation Program!");
 		}
 }
