0efd9dfa341078e61b18b5d38481f59d56c8e0f9
Shubham Phoujdar <sm.phoujdar@gmail.com>
2021-03-10 15:34:19 +0530

    Refactored : Line Length Computation

--- line_comparison/line.java
+++ line_comparison/line.java
@@ -2,4 +2,15 @@ package line_comparison;
 
 public class line {
 
+	double x1 = 0 , y1 = 0 , x2 = 0 , y2 = 0;
+	double length_of_line = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+	public line(double x1, double y1, double x2, double y2) {
+		super();
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		length_of_line = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+	}
+	
 }
--- line_comparison/line_compare.java
+++ line_comparison/line_compare.java
@@ -3,5 +3,10 @@ package line_comparison;
 public class line_compare {
 		public static void main(String[] args) {
 			System.out.println("Welcome to Line Comparison Computation Program!");
+		
+			line line1 = new line(0,0,3,4);
+			
+			System.out.println("Length of provided Line is : " + line1.length_of_line);
+		
 		}
 }
