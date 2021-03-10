42cc04f04c20822500b447ce4e2e270fa696abf2
Shubham Phoujdar <sm.phoujdar@gmail.com>
2021-03-10 15:58:17 +0530

    Refactored : Line Length Computation

--- line_comparison/line_compare.java
+++ line_comparison/line_compare.java
@@ -6,7 +6,10 @@ public class line_compare {
 		
 			line line1 = new line(0,0,3,4);
 			
-			System.out.println("Length of provided Line is : " + line1.length_of_line);
+			System.out.printf("Points Provided - \nP1 [X1,Y1] = [%f,%f] \n"
+					+ "P2 [X1,Y1] = [%f,%f] \n"
+					+ "Length of provided Line is : %f \n",
+					line1.x1, line1.y1, line1.x2, line1.y2, line1.length_of_line);
 		
 		}
 }
